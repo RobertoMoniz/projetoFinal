746936539bbb8b1f3f9710e6677945f85592fc8f
RobertoMoniz <robertomoniz_3@hotmail.com>
2022-03-06 19:37:26 -0300

    Initial commit

---
+++ src/ProjetoJavaGeneration/Funcionario.java
@@ -0,0 +1,69 @@
+package ProjetoJavaGeneration;
+
+public class Funcionario {
+    private String[] nome = {"João", "Paulo", "Pedro", "Jose"};
+    private String[] cpf  = {"434.125.734/19","544.125.114/01","777.121.747/05","889.723.234/33"};
+    private int id;
+    private int compras;
+
+    // CONSTRUTOR
+    public Funcionario(String[] nome, String[] cpf, int compras) {
+
+        this.compras = compras; // Quantidades de vendas realizadas pelo funcionario
+    }
+
+    // GETTERS AND SETTERS
+    public String getNome(int id) {
+        return nome[id];
+    }
+
+    public void setNome(String[] nome) {
+        this.nome[id] = nome[id];
+    }
+
+    public String getCpf(int id) {
+        return cpf[id];
+    }
+
+    public void setCpf(String[] cpf) {
+
+        this.cpf[id] = cpf[id];
+    }
+
+    public int getCompras() {
+
+        return compras;
+    }
+
+    public void setCompras(int compras) {
+
+        this.compras = compras;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    // MÉTODOS
+    //Metodo para checar se o id inserido é valido ou não
+    public boolean isIdValido(int id) {
+        if (id < 0 || id >= cpf.length) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    //modulo para a contagem da comissão [Não finalizado, Douglas já fez no arquivo ProjetoLoja.java, então só precisa pegar de lá e implementar aqui]
+    public void comissao() {
+        setCompras(getCompras()+1);
+    }
+
+}
---
+++ src/ProjetoJavaGeneration/Produto.java
@@ -0,0 +1,45 @@
+package ProjetoJavaGeneration;
+
+/*Classe responsavel por:
+ * - Armazenar o estoque
+ * - Armazenar o preço
+ * - Exibir estoque
+ * - Exibir preço
+ * */
+public class Produto {
+    public Produto (String[] nomeProduto, double[] valorDoProduto) {
+
+    }
+    String[] nomeProduto = {"Pão Francês", "Pão de Leite", "Rosquinha Doce",
+            "Rosquinha Salgada", "Broa de Milho", "Leite 1l",
+            "Cafezinho", "Chocolate Quente"};
+    double[] valorDoProduto = {0.45, 1.00, 1.50, 1.00, 1.25, 5.00, 1.00, 1.50};
+
+    // GETTERS AND SETTERS
+    public String getNomeProduto(int id) {
+        return nomeProduto[id];
+    }
+
+    public void setNomeProduto(String[] nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getValorDoProduto(int id) {
+        return valorDoProduto[id];
+    }
+
+    public void setValorDoProduto(double[] valorDoProduto) {
+        this.valorDoProduto = valorDoProduto;
+    }
+
+
+    // MÉTODOS
+    // Metodo para exibir os produtos no estoque
+    public void exibirProdutos(){
+        System.out.println("ID\t NOME DO PRODUTO \t VALOR");
+
+        for (int i = 0; i < valorDoProduto.length; i++) {
+            System.out.printf("[%d]\t%s\t\t%s\n",i,getNomeProduto(i),getValorDoProduto(i));
+        }
+    }
+}
